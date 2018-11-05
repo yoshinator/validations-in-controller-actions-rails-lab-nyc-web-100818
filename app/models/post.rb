0dc03784d3e55ae3947f9ecd19027114b7b2725a
github username <github email address>
2018-11-04 21:06:51 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid size" }
+  validates :content, length: {minimum: 100}
+
 end
